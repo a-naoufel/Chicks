32fb3860017d925e9c3b8e0ee64b1d5ea86501bc
chaker-yousfi <chaker.yousfi@ensia.edu.dz>
2024-11-04 16:35:08 +0100

    Poussin_mod+entry

--- Game/src/View.java
+++ Game/src/View.java
@@ -22,9 +22,23 @@ public class View extends JComponent implements IObsover {
 
     public void drowPoussin(Graphics g, Poussin poussin) {
         if (poussin.isAlive()) {
+            int x1=((frame.getWidth() * poussin.getX()) / game.gridSizeX())+10;
+            int x3=((frame.getWidth() * poussin.getX()) / game.gridSizeX())+10+poussin.getDirection()*12;
+            int y=((frame.getHeight() - 35) * poussin.getY() / game.gridSizeY())-8  ;
+            int[] XPoints={x1,x1,x3};
+            int[] Ypoints={y-5,y+5,y}; 
             g.setColor(poussin.getColor());
             g.fillOval((frame.getWidth() * poussin.getX()) / game.gridSizeX(),
                     (frame.getHeight() - 35) * poussin.getY() / game.gridSizeY(), 20, 20);
+            g.fillOval(((frame.getWidth() * poussin.getX()) / game.gridSizeX())+3,
+                    ((frame.getHeight() - 35) * poussin.getY() / game.gridSizeY())-14, 15, 15);
+            // g.fillRect((frame.getWidth() * poussin.getX()) / game.gridSizeX(),  (frame.getHeight() - 35) * poussin.getY() / game.gridSizeY(), 20, 20);
+            // g.fillRect((frame.getWidth() * poussin.getX()) / game.gridSizeX(),
+            //        ((frame.getHeight() - 35) * (poussin.getY()) / game.gridSizeY())-19, 15, 15);
+            g.fillPolygon(XPoints, Ypoints, 3);
+                
+            System.out.println(frame.getWidth() * poussin.getX() / game.gridSizeX());
+            System.out.println((frame.getHeight() - 20) * poussin.getY() / game.gridSizeY());
         }
     }
 
@@ -32,10 +46,18 @@ public class View extends JComponent implements IObsover {
         for (int i = 0; i < game.gridSizeX(); i++) {
             for (int j = 0; j < game.gridSizeY(); j++) {
                 g.setColor(game.grid[i][j].getColor());
-                g.fillRect(frame.getWidth() * i / game.gridSizeX(), +(frame.getHeight() - 35) * j / game.gridSizeY(),
+                g.fillRect(frame.getWidth() * i / game.gridSizeX(), (frame.getHeight() - 35) * j / game.gridSizeY(),
                         20, 20);
             }
         }
+        g.setColor(game.getEntry().getColor());
+        int x=game.getEntry().getX();
+        int y=game.getEntry().getY();
+        int X=frame.getWidth() * x / game.gridSizeX();
+        int Y=(frame.getHeight() - 35) * (y-1) / game.gridSizeY();
+        int[] Xs={X,X+20,X+10};
+        int[] Ys={Y,Y, Y+20};
+        g.fillPolygon(Xs, Ys, 3);
     }
 
     @Override
