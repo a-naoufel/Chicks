a5d6703aa67f4bc1c97733ea6600f7cd9e3fbe6b
a-naoufel <n_azizi@estin.com>
2024-11-12 19:23:19 +0100

    add killpoussin ++

--- Game/src/AddPoussins.java
+++ Game/src/AddPoussins.java
@@ -9,6 +9,8 @@ public class AddPoussins extends Thread{
         super.run();
         for (int i = 0; i < 5; i++) {
             game.poussins.add(new Poussin(i,game));
+            Poussin.add();
+            Poussin.displayCounter();
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
--- Game/src/App.java
+++ Game/src/App.java
@@ -2,17 +2,6 @@ public class App {
     public static void main(String[] args){
        Game game = new Game();
        View view = new View(game); 
-
-
-       new Thread(() -> {
-        while (true) {
-            game.updateGame();
-            try {
-                Thread.sleep(200); // adjust for game speed
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }).start();
-   }
+       game.start();
+    }
 }
